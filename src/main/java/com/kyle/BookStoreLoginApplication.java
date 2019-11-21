9a803152df19ea59a4c7a2d6399d53b36f3caa50
kyle <824238868@qq.com>
2019-11-21 16:11:12 +0800

    完成

--- src/main/java/com/kyle/BookStoreLoginApplication.java
+++ src/main/java/com/kyle/BookStoreLoginApplication.java
@@ -2,10 +2,16 @@ package com.kyle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
+@EnableFeignClients
+@EnableRedisHttpSession
 public class BookStoreLoginApplication {
     public static void main(String[] args){
         SpringApplication.run(BookStoreLoginApplication.class);
